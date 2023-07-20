37d1d32fae7550be63776e1b510c0cb0a16f7e1b
Masab Bin Zia <masabmbz5@gmail.com>
2023-07-20 18:12:29 +0500

    finale

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "react-magic-slider-dots/dist/magic-dots.css";
 import TrendingCard from "./TrendingCard";
 import { NftsData } from "@/Data/NftsData";
 
